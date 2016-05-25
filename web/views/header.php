62a87e91692adf0757999042d970a5e2a4c90f39
Tom Yates <tomy8s@hotmail.com>
2016-05-25 21:29:54 +0100

    calendar style for following and previous months

--- web/views/header.php
+++ web/views/header.php
@@ -10,7 +10,7 @@
 <head>
     <title>Tom's site</title>
     <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-    <?php if (basename($_SERVER['REQUEST_URI']) == 'calendar.php') echo '<link rel="stylesheet" type="text/css" href="../stylesheets/calendar.css">
+    <?php if (strpos(basename($_SERVER['REQUEST_URI']),'calendar.php') == 0) echo '<link rel="stylesheet" type="text/css" href="../stylesheets/calendar.css">
     ';?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
