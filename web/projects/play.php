506acb7d00e975fce946cfd92b57358e1f4b40af
Tom Yates <tomy8s@hotmail.com>
2016-05-30 13:21:55 +0100

    fixed date bug for looking at Feb in the last days of months in a leap-year

--- web/projects/play.php
+++ web/projects/play.php
@@ -1,20 +1,20 @@
 <table><tr><td>
 <?php
-echo strtotime("now"), "<br>";
-echo strtotime("10 September 2000"), "<br>";
-echo strtotime("2000-09-10"), "<br>";
-echo strtotime("+1 day"), "<br>";
-echo strtotime("+1 week"), "<br>";
-echo strtotime("+1 week 2 days 4 hours 2 seconds"), "<br>";
-echo strtotime("next Thursday"), "<br>";
-echo strtotime("last Monday"), "<br>";
-?></td><td><?php
-echo 'strtotime("now")', "<br>";
-echo 'strtotime("10 September 2000")', "<br>";
-echo 'strtotime("2000-09-10")', "<br>";
-echo 'strtotime("+1 day")', "<br>";
-echo 'strtotime("+1 week")', "<br>";
-echo 'strtotime("+1 week 2 days 4 hours 2 seconds")', "<br>";
-echo 'strtotime("next Thursday")', "<br>";
-echo 'strtotime("last Monday")', "<br>";
-?></td></tr></table>
+echo date('F',mktime(0,0,0,1)).'<br>';
+echo date('F',mktime(0,0,0,2)).'<br>';
+echo date('F',mktime(0,0,0,3)).'<br>';
+echo date('F',mktime(0,0,0,4)).'<br>';
+echo date('F',mktime(0,0,0,5)).'<br>';
+echo date('F',mktime(0,0,0,6)).'<br>';
+echo date('F',mktime(0,0,0,7)).'<br>';
+echo date('F',mktime(0,0,0,8)).'<br>';
+echo date('F',mktime(0,0,0,9)).'<br>';
+echo date('F',mktime(0,0,0,10)).'<br>';
+echo date('F',mktime(0,0,0,11)).'<br>';
+echo date('F',mktime(0,0,0,12)).'<br>';
+echo mktime(0,0,0,1).'<br>';
+echo mktime(0,0,0,2).'<br>';
+echo mktime(0,0,0,3).'<br>';
+echo date('F',1454108400).'<br>';
+echo date('d F',1456786800).'<br>';
+echo date('d F',1459288800).'<br>';
